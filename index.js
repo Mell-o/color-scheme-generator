7709d26c9591781b2d7a829a31ece0795fa5c280
Mellow <95871222+Mell-o@users.noreply.github.com>
2026-09-15 11:54:48 -0400

    fetch color scheme data and prepare display functions
    Created the API request URL and added logic to fetch the color scheme data. Added placeholder functions for displaying the colors and hex values. Also added a name attribute to the color input and updated the select element’s name value.

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 import {schemeModes} from "./data.js"
 
 const schemeModesSelect = document.querySelector('.scheme-modes-select')
+const myForm = document.querySelector("form")
+
 
 function addSchemeModes(schemeModes){
     schemeModes.forEach((mode) => {
         schemeModesSelect.innerHTML += `<option value="${mode.toLowerCase()}">${mode}</option>`
     })
 }
+
+myForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    
+    const formData = new FormData(event.target)
+
+    const schemeFormData = {
+        "hex": formData.get("scheme-color"),
+        "mode": formData.get("scheme-mode"),
+    }
+
+    const requestURL = getRequestURL(schemeFormData.hex, schemeFormData.mode)
+
+    loadColors(requestURL)
+})
+
+function getRequestURL(hex, mode){
+    const baseURL = "https://www.thecolorapi.com"
+    const endPoint = "/scheme"
+    const queryString = `?hex=${hex.slice(1)}&mode=${mode}`
+
+    return `${baseURL}${endPoint}${queryString}`
+}
+
+function loadColors(requestURL){
+
+    return fetch(requestURL)
+        .then(response => response.json())
+        .then(data => {
+            const colors = data.colors.map(({ hex: { clean } }) => clean)
+            
+            displayColors(colors)
+
+            displayHexes(colors)
+        })
+}
+
+function displayColors(colors){
+}
+
+function displayHexes(colors){
+}
+
 
 addSchemeModes(schemeModes)
